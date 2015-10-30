69a04458e9ba28f2b5dbd6114c188e70a599efc1
Guillermo Martín <xguillemartinx@gmail.com>
2015-10-30 00:59:51 -0300

    Definición estructura de clase.

--- YoComparo/src/com/yoComparo/bussiness/CoreBO.java
+++ YoComparo/src/com/yoComparo/bussiness/CoreBO.java
@@ -1,5 +1,33 @@
 package com.yoComparo.bussiness;
 
+import java.util.List;
+
+import com.yoComparo.model.Producto;
+
 public class CoreBO {
-	String aaa = "";
+	private String id;
+	private String descripción;
+	private List<Producto> productos;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescripción() {
+		return descripción;
+	}
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	public void compararProductos(String id){
+		
+	}
 }
